6235225cf24db63c7d8a3423fab8976600f4b792
Andrew Schell <paintstone@gmail.com>
2021-05-22 07:44:11 -0700

    added output comment
    # output is 21 because (4) words + (3) commas + carriage return

--- csv_writer.py
+++ csv_writer.py
@@ -1,5 +1,6 @@
-#%%
+
 import csv
 outputFile = open('output.csv', 'w', newline='')
 outputWriter = csv.writer(outputFile)
 outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham'])
+# output is 21 because (4) words + (3) commas + carriage return
