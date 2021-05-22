27e295d2f2ed9bb255fbbc553cfed1f4dd710fc7
Andrew Schell <paintstone@gmail.com>
2021-05-22 07:47:47 -0700

    updated sample output types
    added hello world, numbers and closed

--- csv_writer.py
+++ csv_writer.py
@@ -1,6 +1,19 @@
+#%%
 
 import csv
 outputFile = open('output.csv', 'w', newline='')
 outputWriter = csv.writer(outputFile)
 outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham'])
 # output is 21 because (4) words + (3) commas + carriage return
+#%%
+
+outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+#%%
+
+outputWriter.writerow([1, 2, 3,141592, 4])
+#%%
+
+outputFile.close()
+#%%
+
+
